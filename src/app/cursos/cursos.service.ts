2b74182419967c99e507e842a515bebb1b99306f
Diogo <cirnediogo@gmail.com>
2017-11-08 11:13:16 -0300

    Aula #07: Serviços (Services) e Injeção de dependência (DI)

---
+++ src/app/cursos/cursos.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CursosService {
+
+  cursos: string[];
+
+  constructor() { 
+    this.cursos = ['Java', 'C++', 'Angular', 'Node'];
+  }
+
+  getCursos() {
+    return this.cursos;
+  }
+
+}
