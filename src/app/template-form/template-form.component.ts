40572a0677777d292f50712348297951d5bdc5d7
Diogo Cirne <cirnediogo@gmail.com>
2017-11-14 13:07:45 -0300

    Aula #78: Forms (template driven) Aplicando CSS na validação dos campos

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: '',
-    email: ''
+    nome: null,
+    email: null
   }
 
   constructor() { }
