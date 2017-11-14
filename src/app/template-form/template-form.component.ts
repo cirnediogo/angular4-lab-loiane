b4462cbd7b993e4cb0890190cc328aeb9debb9c3
Diogo Cirne <cirnediogo@gmail.com>
2017-11-14 13:00:16 -0300

    Aula #77: Forms (template driven) Aplicando validação nos campos

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: "Diogo",
-    email: "diogo@email.com"
+    nome: '',
+    email: ''
   }
 
   constructor() { }
