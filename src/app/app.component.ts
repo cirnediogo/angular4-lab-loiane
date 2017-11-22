c0f8e53bc3e11ee5143a0cc96c07ac04a16b3a1a
Diogo Cirne <cirnediogo@gmail.com>
2017-11-22 14:10:55 -0300

    Aula #100: Formulários: Criando um serviço de Estados Brasileiros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  section = 'data-form';
+  section = 'intro';
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
-import { FormDebugComponent } from './form-debug/form-debug.component';
-import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FormDebugComponent } from './form-debug/form-debug.component';
+import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
+import { DropdownService } from './services/dropdown.service';
 
 @NgModule({
   imports: [
@@ -14,6 +16,9 @@ import { CommonModule } from '@angular/common';
   exports: [
     FormDebugComponent,
     CampoControlErroComponent
+  ],
+  providers: [
+    DropdownService
   ]
 })
 export class SharedModule { }
