0580b1909cfa23b36a3085d47452f2713f443678
Diogo Cirne <cirnediogo@gmail.com>
2017-11-22 12:35:39 -0300

    Aula #94: Formulários reativos: Aplicando validação nos campos

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Http } from '@angular/http';
 
 @Component({
@@ -24,8 +24,8 @@ export class DataFormComponent implements OnInit {
     // });
     /* Opção por FormBuilder */
     this.formulario = this.formBuilder.group({
-        nome: [null],
-        email: [null]
+        nome: [null, Validators.required],
+        email: [null, [Validators.required, Validators.email]]
     })
   }
 
